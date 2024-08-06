42c125cafc7dd44444e9b7baa51545562901d755
Priyanka S <113746848+PRIYANKA2272004@users.noreply.github.com>
2024-08-06 18:36:18 +0530

    Create BookService.java

---
+++ week2/Spring_Core_Maven/Exercise 9 - Creating a Spring Boot Application/BookService.java
@@ -0,0 +1,23 @@
+package com.library.service;
+
+import com.library.entity.Book;
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+}
